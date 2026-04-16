96d8e1c5ffcbc5d8ebc8fc752e651e61f05d6114
matiaslopezsosaff-commits <matiaslopezsosaff@gmail.com>
2026-04-16 12:21:49 -0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,17 @@
+const mangas = [
+  {
+    id: "solo",
+    title: "Mi Manhwa",
+    cover: "images/nombre-manhwa/cap1/001.jpg",
+    chapters: [
+      {
+        number: 1,
+        images: [
+          "images/nombre-manhwa/cap1/001.jpg",
+          "images/nombre-manhwa/cap1/002.jpg",
+          "images/nombre-manhwa/cap1/003.jpg"
+        ]
+      }
+    ]
+  }
+];
